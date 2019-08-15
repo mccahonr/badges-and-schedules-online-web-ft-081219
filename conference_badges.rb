b3acee59c66a95f2595b0065a05fcc2b3aab9fa1
Ryan McCahon <mccahonr@yahoo.com>
2019-08-15 14:57:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def badge_maker(name)
+  badge = "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  people = []
+  badges = []
+  people.each do |name|
+    badges.push("Hello, my name is #{name}.")
+  end
+  badges
+end
